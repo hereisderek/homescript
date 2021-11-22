490edb851a370af735ba9aae0a7a6358b3ff54ee
Derek Zhu <1and1get2@gmail.com>
2021-11-23 08:56:30 +1300

    add profiles, add torrentvpn

--- dockers/run.sh
+++ dockers/run.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/bash
-export PROFILE_LOCATION=profiles/ubuntu
-ENV_FILE=${PROFILE_LOCATION}/.env
-DOCKER_COMPOSE_OVERRIDE_FILE=${PROFILE_LOCATION}/docker-compose.override.yml
+cd "$(dirname "$0")"
 
-_env_=; [[ -f $ENV_FILE ]]&&_env_="--env-file ${ENV_FILE}"
-_docker_compose_override_=; [[ -f $DOCKER_COMPOSE_OVERRIDE_FILE ]]&&_docker_compose_override_="-f docker-compose.yml -f ${DOCKER_COMPOSE_OVERRIDE_FILE}"
+dockers() {
+    DEFAULT_PROFILE=profiles/ubuntu
+    PROFILE_LOCATION=${PROFILE_LOCATION:-${DEFAULT_PROFILE}}
+    
+    PROFILE_ENV_FILE=${PROFILE_LOCATION}/.env
+    DOCKER_COMPOSE_OVERRIDE_FILE=${PROFILE_LOCATION}/docker-compose.override.yml
 
-echo "_env_:${_env_} "
-echo "_docker_compose_override_:${_docker_compose_override_}"
+    compose_override_params=;[ -f $DOCKER_COMPOSE_OVERRIDE_FILE ]&&compose_override_params=" -f docker-compose.yml -f ${DOCKER_COMPOSE_OVERRIDE_FILE}"
 
-alias dockers="docker-compose ${_docker_compose_override_} ${_env_}"
+    merge_env_files() {
+        # echo "merging ${@:2} into ${1}..."
+        local env_override_file=${1}
+        mkdir -p $(dirname ${1})
+        # awk -F= '{a[$1]=$2}END{for(i in a) print i "=" a[i]}' "${@:2}">${1}
+        sort -u -t '=' -k 1,1 $(echo "${@:2}"|tr ' ' '\n'|tac|tr '\n' ' ')>${1}
+    }
 
-
-
-return 0
+    env_params=
+    if [ -f .env ] && [ -f $PROFILE_ENV_FILE ]; then
+        echo "both .env and $PROFILE_ENV_FILE exist, merging together..."
+        env_override_file=${PROFILE_LOCATION}/env.import
+        merge_env_files $env_override_file .env $PROFILE_ENV_FILE
+        eval docker-compose ${compose_override_params} --env-file $env_override_file $@
+    elif [ -f $PROFILE_ENV_FILE ]; then
+        echo "use $PROFILE_ENV_FILE"
+        eval docker-compose "$compose_override_params --env-file $PROFILE_ENV_FILE $@"
+    else 
+        echo ".env not speicified"
+        eval docker-compose $@
+    fi
+}
---
+++ dockers/scripts/upload_media.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Exit if running
+if [[ $(pidof -x "$(basename "$0")" -o %PPID) ]]; then
+echo "Already running, exiting..."; exit 1; fi
+
+
+export RCLONE_CONFIG_DIR=/dockers/config/rclone/config
+DOCKER_CONFIG_DIR=/config/rclone
+
+EXCLUDE_FILE_NAME=exclude.txt
+CONFIG_FILE_NAME=rclone.conf
+
+LOCAL=/media/data/storage
+# REMOTES="share_04 share_03 share_02 share_01"
+REMOTES="share_02 share_04 share_03 share_02"
+REMOTE_MEDIA_PATH_RELATTIVE="/Shared/Media"
+
+
+# is $LOCAL exist
+[[ ! -d $LOCAL ]] && {
+    echo "unable to find local folder:${LOCAL}, exiting..."; exit 1;
+}
+
+# Is $LOCAL actually a local disk?
+if /bin/findmnt $LOCAL -o FSTYPE -n | grep fuse; then
+echo "FUSE file system found, exiting..."; exit 1; fi
+
+
+
+
+# Check for excludes file
+if [[ ! -f ${RCLONE_CONFIG_DIR}/${EXCLUDE_FILE_NAME} ]]; then
+echo "excludes file not found, aborting..."; exit 1; fi
+
+docker_exclude="--exclude-from ${DOCKER_CONFIG_DIR}/${EXCLUDE_FILE_NAME}"
+
+
+for remote in ${REMOTES}; do
+    remote_location="${remote}:${REMOTE_MEDIA_PATH_RELATTIVE}"
+    echo "using remote: $remote, ${LOCAL} -> ${remote_location}"
+
+    docker_name="rclone_sync_${remote}"
+    docker rm $docker_name
+    docker run --name ${docker_name} --rm -ti \
+        --volume ${RCLONE_CONFIG_DIR}:${DOCKER_CONFIG_DIR} \
+        --volume ${LOCAL}:/data \
+        --user $(id -u):$(id -g) \
+        rclone/rclone:beta \
+        move /data/ ${remote_location}  --delete-empty-src-dirs  \
+        -v -P ${docker_exclude}  --transfers 8 --checkers 8 \
+        --fast-list --drive-stop-on-upload-limit --delete-during \
+	--min-age 0h  --drive-chunk-size=128M --max-transfer 700G \
+
+
+    error_code=$?
+
+    # try different remote if hit upload limit
+    case $error_code in
+        7|8)
+            echo "fatal error $error_code retrying with different end point"
+            continue
+            ;;
+        0|9)
+            echo "finished"
+            exit 0
+            ;;
+        *)
+            echo "error $error_code encountered, exiting..."
+            exit $error_code
+            ;;
+    esac
+done
+
+
+# Move older local files to the cloud...
+# I added in 3 days to let the files sit a few days so Plex intro anaylsis can happen locally
+# /usr/bin/rclone move $LOCAL gcrypt: --log-file /opt/rclone/logs/upload.log -v --exclude-from /opt/rclone/scripts/excludes --delete-empty-src-dirs --fast-list --drive-stop-on-upload-limit --min-age 3d
